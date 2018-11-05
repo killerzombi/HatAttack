3dc60b59e4e4a77c151af2101d7df9ffd7a88d0f
evan0101 <supdeerlord@gmail.com>
2018-11-05 13:57:37 -0500

    Added CombatGridCreator + UnitController - EM
    Added a basic grid creator script to generate a 30x30 combat scene grid. Added a Unit Controller script to control a test unit (sphere) that allows movement from node to node using WASD keys.

---
+++ HatAttack/Assets/Test Scenes/Test Scripts (Evan)/CombatGridCreator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatGridCreator : MonoBehaviour
+{
+
+  public GameObject cube;
+
+
+  // Width, Length
+  private GameObject[,] grid = new GameObject[30, 30];
+
+  void Start()
+  {
+    createCombatMap();
+  }
+
+  void createCombatMap()
+  {
+    for (int x = 0; x < grid.GetLength(0); x++)
+    {
+      for (int z = 0; z < grid.GetLength(1); z++)
+      {
+        // Creates block.
+        GameObject block = Instantiate(cube, Vector3.zero, cube.transform.rotation) as GameObject;
+        block.transform.parent = transform;
+        block.transform.localPosition = new Vector3(x, 0, z);
+        // Sets block object to it's position in the array so we can access it.
+        grid[x, z] = block;
+      }
+    }
+  }
+
+  public GameObject[,] getGrid()
+  {
+    return grid;
+  }
+
+}
---
+++ HatAttack/Assets/Test Scenes/Test Scripts (Evan)/UnitController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour
+{
+
+  public CombatGridCreator combatGridCreator;
+
+  private GameObject[,] grid;
+
+  public float t;
+  Vector3 startPosition;
+  Vector3 target;
+  float timeToReachTarget;
+
+  private int uX = 0;
+  private int uZ = 0;
+
+  public bool isMoving = false;
+
+  void Start()
+  {
+    grid = combatGridCreator.getGrid();
+    target = transform.position;
+  }
+
+
+  public IEnumerator MoveToPosition(Vector3 target, float timeToMove)
+  {
+    var currentPos = this.transform.position;
+    var t = 0f;
+    while (t < 1)
+
+    {
+      t += Time.deltaTime / timeToMove;
+      this.transform.position = Vector3.Lerp(currentPos, target, t);
+      isMoving = false;
+      yield return null;
+    }
+  }
+
+
+  void Update()
+  {
+
+    if (Input.GetKeyDown("w"))
+    {
+      uZ++;
+      if (!isMoving)
+      {
+        isMoving = true;
+        StartCoroutine(MoveToPosition(grid[uX, uZ].transform.position + new Vector3(0, 1, 0), 0.1f));
+      }
+    }
+
+    if (Input.GetKeyDown("s"))
+    {
+      if (!isMoving)
+      {
+        uZ--;
+        isMoving = true;
+        StartCoroutine(MoveToPosition(grid[uX, uZ].transform.position + new Vector3(0, 1, 0), 0.1f));
+      }
+
+    }
+
+    if (Input.GetKeyDown("d"))
+    {
+      if (!isMoving)
+      {
+        uX++;
+        isMoving = true;
+        StartCoroutine(MoveToPosition(grid[uX, uZ].transform.position + new Vector3(0, 1, 0), 0.1f));
+      }
+    }
+
+    if (Input.GetKeyDown("a"))
+    {
+      if (!isMoving)
+      {
+        uX--;
+        isMoving = true;
+        StartCoroutine(MoveToPosition(grid[uX, uZ].transform.position + new Vector3(0, 1, 0), 0.1f));
+      }
+    }
+  }
+}
