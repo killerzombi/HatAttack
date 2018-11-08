9bde8900f592159ed4ccdf1143e54444aa5534f5
AinTheBrain <Ian818@yahoo.com>
2018-11-08 15:32:57 -0500

    Hopefully this works

--- HatAttack/Assets/Test Scenes/Test Scripts (Evan)/CombatGridCreator.cs
+++ HatAttack/Assets/Test Scenes/Test Scripts (Evan)/CombatGridCreator.cs
@@ -10,21 +10,67 @@ public class CombatGridCreator : MonoBehaviour
 
   public GameObject TerrainType;
 
+  const int gridSizeX = 30;
+  const int gridSizeZ = 30;
 
+    int baseSpawnSize = 5;
+    
   // Width, Length
-  private GameObject[,] grid = new GameObject[30, 30];
+  private GameObject[,] grid = new GameObject[gridSizeX, gridSizeZ];
+  private bool[,] waterPlacer = new bool[gridSizeX, gridSizeZ];
+  
 
   void Start()
   {
+    generateWaterSpots();
     createCombatMap();
     createGridEffect();
   }
 
+  void generateWaterSpots()
+    {
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int z = 0; z < gridSizeZ; z++)
+            {
+                if (x <= 8 && z <= 8)
+                {
+                    waterPlacer[x, z] = false;
+                }
+                else if (x >= gridSizeX - 8 && z >= gridSizeZ - 8)
+                {
+                    waterPlacer[x, z] = false;
+                }
+                else if (x <= 3 || x >= gridSizeX - 3)
+                {
+                    waterPlacer[x, z] = false;
+                }
+                else if (z <= 3 || z >= gridSizeZ - 3)
+                {
+                    waterPlacer[x, z] = false;
+                }
+                else
+                {
+                    waterPlacer[x, z] = true;
+                }
+            }
+        }
+        int waterNum = 0;
+        int waterTryCount = 0;
+        int xPos;
+        int zPos;
+        while (waterNum <= 3 || waterTryCount <= 8)
+        {
+            xPos = Random.Range(1, gridSizeX);
+            zPos = Random.Range(1, gridSizeZ);
+        }
+    }
+
   void createCombatMap()
   {
-    for (int x = 0; x < grid.GetLength(0); x++)
+    for (int x = 0; x < gridSizeZ; x++)
     {
-      for (int z = 0; z < grid.GetLength(1); z++)
+      for (int z = 0; z < gridSizeX; z++)
       {
         // Calls TerrainType and returns us a random terrain type block.
         GameObject block = Instantiate(GetComponent<TerrainType>().randomizer(), Vector3.zero, cube.transform.rotation) as GameObject;
@@ -42,9 +88,9 @@ public class CombatGridCreator : MonoBehaviour
   void createGridEffect()
   {
     int offset = 0;
-    for (int x = 0; x < grid.GetLength(0); x++)
+    for (int x = 0; x < gridSizeX; x++)
     {
-      for (int z = 0; z < grid.GetLength(1); z++)
+      for (int z = 0; z < gridSizeZ; z++)
       {
         // Calls the randomizer function and returns a random block.
         if (offset % 2 == 0)
