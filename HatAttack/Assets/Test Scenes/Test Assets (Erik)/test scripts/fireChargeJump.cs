a9cbf5b163a52324931d7b26537110bf312b0820
CyborgDuck <erik.m.english@gmail.com>
2018-11-16 22:27:05 -0500

    FireChargeJump
    made the ability in my test scene a script to let the player jump when they switch to the char with that ability to jump higher the longer they hold space bar.

---
+++ HatAttack/Assets/Test Scenes/Test Assets (Erik)/test scripts/fireChargeJump.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireChargeJump : MonoBehaviour {
+
+    private bool onGround;
+    private float jumpPressure;
+    private float minJump;
+    private float maxJumpPressure;
+    private Rigidbody rbody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        onGround = true;
+        jumpPressure = 0f;
+        minJump = 2f;
+        maxJumpPressure = 10f;
+        rbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(onGround)
+        {
+            //holding jump button
+            if(Input.GetButton("Jump"))
+            {
+                if(jumpPressure < maxJumpPressure)
+                {
+                    jumpPressure += Time.deltaTime * 10f;
+                }
+                else
+                {
+                    jumpPressure = maxJumpPressure;
+                }
+            }
+            //not holding jump button
+            else
+            {
+                if(jumpPressure > 0f)
+                {
+                    jumpPressure = jumpPressure + minJump;
+                    rbody.velocity = new Vector3(jumpPressure/10f, jumpPressure, 0f);
+                    jumpPressure = 0f;
+                    onGround = false;
+                }
+            }
+        }
+	}
+
+    //need to tag the plane or terrain as ground
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("ground"))
+        {
+            onGround = true;
+        }
+    }
+
+}
