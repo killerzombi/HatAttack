2f6ea72c937b4d6d6aee3aca5c079503e48e018c
CyborgDuck <erik.m.english@gmail.com>
2018-11-18 13:37:39 -0500

    no real progress :(
    made a showcase of the animals that are in the package were using, and looked at a few effects that might be worth using.
    
    attempting to make a hose of ice that when touched unrendered blocks will make them revealed or re-rendered. Need to study for a calculus test for now though, this was what I was working off of, https://unity3d.com/learn/tutorials/lets-try/shooting-with-raycasts?playlist=41639

--- HatAttack/Assets/Test Scenes/Test Assets (Erik)/test scripts/spawnIceBlock.cs
+++ HatAttack/Assets/Test Scenes/Test Assets (Erik)/test scripts/spawnIceBlock.cs
@@ -4,16 +4,59 @@ using UnityEngine;
 
 public class spawnIceBlock : MonoBehaviour
 {
+    public float fireRate = .25f;
+    public float weaponRange = 50f;
+    public float hitForce = 100f;
+    public Transform gunEnd;
+    private Camera fpsCam;
+    private WaitForSeconds shotDuration = new WaitForSeconds(.07f);
+    private AudioSource gunAudio;
+    private LineRenderer laserLine;
+    private float nextFire;
+
 
 	// Use this for initialization
 	void Start ()
     {
-		
+        laserLine = GetComponent<LineRenderer>();
+        gunAudio = GetComponent<AudioSource>();
+        fpsCam = GetComponentInParent<Camera>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+		if(Input.GetMouseButton(0) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            StartCoroutine(ShotEffect());
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(.5f, .5f, 0));
+
+            RaycastHit hit;
+
+            laserLine.SetPosition(0, gunEnd.position);
+
+            if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, weaponRange))
+            {
+                laserLine.SetPosition(1, hit.point);
+            }
+            else
+            {
+                laserLine.SetPosition(1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+            }
+        }
+        
+
+
+    }
+
+    private IEnumerator ShotEffect()
+    {
+        gunAudio.Play();
+        laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
+    }
+
+    
 }
