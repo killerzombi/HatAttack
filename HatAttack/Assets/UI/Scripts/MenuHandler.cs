0fa3389c840a4c6a46e1142ce078afdaf4fbfc2a
evan0101 <supdeerlord@gmail.com>
2018-11-19 11:59:22 -0500

    Updated file structure for Menu Screens - EM
    Restructured the menu screen UI in the heirachy to make more sense.

---
+++ HatAttack/Assets/UI/Scripts/MenuHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+
+  public void exitGame()
+  {
+    Application.Quit();
+  }
+
+  // public void playMyGame()
+  //   {
+  //       //Quick and simple way to load a new scene
+  //       SceneManager.LoadScene("Level01");
+  //   }
+
+  //   public void exitTheGame()
+  //   {
+  //       Application.Quit();
+  //   }
+}
