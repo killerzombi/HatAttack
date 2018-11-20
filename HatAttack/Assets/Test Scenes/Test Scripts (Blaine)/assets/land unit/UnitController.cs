c9ef5b652baa661cb8556dc9000d93149e23ef24
killerzombi <flamester41@gmail.com>
2018-11-20 17:45:16 -0500

    more minor changes! -BH

---
+++ HatAttack/Assets/Test Scenes/Test Scripts (Blaine)/assets/land unit/UnitController.cs
@@ -0,0 +1,274 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour, UnitControllerInterface, SelectionInterface
+{
+
+    private float health;
+    private float attack;
+    private float defense;
+    private int moveSpeed = 5;
+    private float moveTime = 1f;
+
+
+    private MapInterface MInterface;
+    private Vector2Int position;
+    private bool MovingNow = false;
+    private bool inMoveSys = false;
+    private Queue<Queue<Vector2Int>> allPaths = null;
+    private Queue<Vector2Int> allPathPositions = null;
+    private Queue<Queue<Vector2Int>> nextPaths = null;
+    private Queue<Vector2Int> nextPathPositions = null;
+
+    private event TickManager.Tick tick;
+
+    private event TickManager.Tick moveNow;
+    private int moveNowCount = 0;
+
+
+    private MeshRenderer MeshR;
+
+    private IEnumerator MoveDownPath(Queue<Vector2Int> path, float timeToMove)
+    {
+        float cTimeToMove = timeToMove / path.Count;
+        while (path.Count > 0)
+        {
+            Vector2Int cpos = path.Dequeue();
+
+            Vector3 target = MInterface.GetNode(cpos.x, cpos.y).transform.position;
+            //Debug.Log(target);
+            Vector3 currentPos = this.transform.position;
+            float t = 0f;
+            while (t < 1)
+            {
+                t += Time.deltaTime / cTimeToMove;
+                this.transform.position = Vector3.Lerp(currentPos, target, t);
+                yield return null;
+            }
+
+        }
+        MovingNow = false;
+        moveNow();
+    }
+
+    private void NextMove()
+    {
+        moveNowCount--;
+        if (!MovingNow)
+        {
+            MovingNow = true;
+            StartCoroutine(MoveDownPath(nextPaths.Dequeue(), moveTime));
+            position = nextPathPositions.Dequeue();
+        }
+        else
+            moveNowCount++;
+
+        if (moveNowCount <= 0) { moveNow -= NextMove; }
+    }
+
+    public void MoveUnit(Vector2Int target)
+    {
+        Queue<Vector2Int> Path = MInterface.getPath(target.x, target.y);
+        if (Path.Count > 0)
+        {
+
+            while (Path.Count > moveSpeed)
+            {
+                Queue<Vector2Int> cPath = new Queue<Vector2Int>();
+                for (int i = 0; i < moveSpeed - 1; i++)
+                    cPath.Enqueue(Path.Dequeue());
+                Vector2Int cTarget = Path.Dequeue();
+
+                allPaths.Enqueue(cPath);
+                allPathPositions.Enqueue(cTarget);
+            }
+
+            allPaths.Enqueue(Path);
+            allPathPositions.Enqueue(target);
+        }
+        if (!inMoveSys)
+        {
+            switch (TickManager.instance.getTM())
+            {
+                case TickManager.TickMode.Chaos:
+                    {
+                        TickManager.tick += moveOnTick;
+                        inMoveSys = true;
+                        break;
+                    }
+                case TickManager.TickMode.Team:
+                    {
+                        inMoveSys = true;
+                        break;
+                    }
+                case TickManager.TickMode.Initiative1:
+                    {
+                        tick += moveOnTick;
+                        inMoveSys = true;
+                        break;
+                    }
+                case TickManager.TickMode.Initiative2:
+                    {
+                        inMoveSys = true;
+                        break;
+                    }
+            }
+        }
+    }
+
+    private void moveOnTick()  //IF YOU CHANGE THE NAME CHANGE IT IN MOVEUNIT AS WELL!!!
+    {
+        if (allPaths.Count > 0)
+        {
+            Queue<Vector2Int> path = new Queue<Vector2Int>(allPaths.Dequeue());
+            if (!MovingNow)
+            {
+                MovingNow = true;
+                StartCoroutine(MoveDownPath(path, moveTime));
+                position = allPathPositions.Dequeue();
+            }
+            else
+            {
+                nextPaths.Enqueue(path);
+                nextPathPositions.Enqueue(allPathPositions.Dequeue());
+                if (moveNowCount == 0)
+                    moveNow += NextMove;
+                moveNowCount++;
+            }
+        }
+        else
+        {
+            switch (TickManager.instance.getTM())
+            {
+                case TickManager.TickMode.Chaos:
+                    {
+                        TickManager.tick -= moveOnTick;
+                        inMoveSys = false;
+                        break;
+                    }
+                case TickManager.TickMode.Team:
+                    {
+                        inMoveSys = false;
+                        break;
+                    }
+                case TickManager.TickMode.Initiative1:
+                    {
+                        tick -= moveOnTick;
+                        inMoveSys = false;
+                        break;
+                    }
+                case TickManager.TickMode.Initiative2:
+                    {
+                        inMoveSys = false;
+                        break;
+                    }
+            }
+        }
+    }
+
+    public Queue<Vector2Int> pathFrom(Vector2Int startingPoint)
+    {
+        if (MInterface != null) return MInterface.PathAtoB(startingPoint, position);
+        else Debug.Log("CombatGridCreator not found");
+        return new Queue<Vector2Int>();
+    }
+
+    public void setGrid(MapInterface cgc, Vector2Int pos)
+    {
+        MInterface = cgc; position = pos; Initialize();
+    }
+
+    public void highlightGrid(Color C, Vector2Int pos)
+    {
+        if (MInterface != null) MInterface.startHighlight(pos.x, pos.y, C, moveSpeed);
+        else Debug.Log("CombatGridCreator not found");
+    }
+
+    public void highlightGrid(Color C)
+    {
+        if (MInterface != null) MInterface.startHighlight(position.x, position.y, C, moveSpeed);
+        else Debug.Log("CombatGridCreator not found");
+    }
+    public void unHighlightGrid()
+    {
+        if (MInterface != null) MInterface.unHighlight();
+        else Debug.Log("CombatGridCreator not found");
+    }
+
+    public void selected(Color C)
+    {
+        //Debug.Log(C);
+        if (MeshR != null)
+        {
+            MeshR.material.EnableKeyword("_EMISSION");
+            MeshR.material.SetColor("_EmissionColor", C);
+        }
+        else Debug.Log("no MeshRenderer");
+    }
+
+    public void deselected()
+    {
+        if (MInterface != null) MInterface.unHighlight();
+        else Debug.Log("CombatGridCreator not found");
+        if (MeshR != null)
+        {
+            MeshR.material.DisableKeyword("_EMISSION");
+        }
+        else Debug.Log("no MeshRenderer");
+    }
+
+    public Vector2Int getPosition()
+    {
+        return position;
+    }
+
+    public void Initialize()
+    {
+        moveNowCount = 0;
+        if (MeshR == null)
+        {
+            MeshR = GetComponent<MeshRenderer>();
+            if (MeshR == null) Debug.Log("no MeshRenderer");
+        }
+        allPaths = new Queue<Queue<Vector2Int>>();
+        allPathPositions = new Queue<Vector2Int>();
+        nextPaths = new Queue<Queue<Vector2Int>>();
+        nextPathPositions = new Queue<Vector2Int>();
+        if (TickManager.instance != null)
+        {
+            switch (TickManager.instance.getTM())
+            {
+                case TickManager.TickMode.Chaos:
+                    {
+                        break;
+                    }
+                case TickManager.TickMode.Team:
+                    {
+                        break;
+                    }
+                case TickManager.TickMode.Initiative1:
+                    {
+                        TickManager.instance.EnqueuePlayer(tick);
+                        break;
+                    }
+                case TickManager.TickMode.Initiative2:
+                    {
+                        break;
+                    }
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Initialize();
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+}
--- HatAttack/Assets/Test Scenes/Test Scripts (Blaine)/scripts/ArrayScriptCombat.cs
+++ HatAttack/Assets/Test Scenes/Test Scripts (Blaine)/scripts/ArrayScriptCombat.cs
@@ -10,7 +10,6 @@ public class ArrayScriptCombat : MonoBehaviour, MapInterface
     public GameObject Unit4;
     [Range(0.25f,15f)]public float tickDelay = 3f;
     [SerializeField] private bool noTimer = false;
-    //public bool easyMode = false;
 
     // =============================
     // Terrain Types
@@ -20,14 +19,7 @@ public class ArrayScriptCombat : MonoBehaviour, MapInterface
     public GameObject tree;
     public GameObject rock;
     public GameObject bush;
-
-
-    //private GameObject[,] map;
-    //private MeshRenderer[,] mapM;
-    //private bool[,] danger;  //is this cube  snake?
-    //private int MW, ML;  //map length and width
-    //private Material nextBack;
-    //private Vector2Int NBposition;
+    
 
 
     const int gridSizeX = 30;
@@ -40,81 +32,8 @@ public class ArrayScriptCombat : MonoBehaviour, MapInterface
     // Use this for initialization
     void Start()
     {
-        //nextBack = null;
-        //Vector2 topLeft = new Vector2((float)-((width + ((width - 1) * spacing)) / 2), (float)((height + ((height - 1) * spacing)) / 2));
-        //ML = width;
-        //MW = height;
-        //Debug.Log("Array: H:" + height + "  W:" + width + "  ML:" + ML + "  MW:" + MW);
-        //map = new GameObject[width, height];
-        //mapM = new MeshRenderer[ML, MW];
-        //danger = new bool[ML, MW];
-        //for(int x = 0; x< width; x++)
-        //{
-        //    for (int y = 0; y < height; y++)
-        //    {
-        //        map[x, y] = (GameObject)Instantiate(prefabCube, new Vector3(topLeft.x + x + x * spacing, 0f, topLeft.y - y - y * spacing), Quaternion.identity, this.gameObject.transform);
-        //        mapM[x, y] = map[x, y].GetComponent<MeshRenderer>();
-        //        danger[x, y] = false;
-        //    }
-        //}
 
-        //foreach (MeshRenderer M in mapM)
-        //{
-        //    M.material = Ground;
-        //}
-
-        TerrainType TT = this.gameObject.AddComponent<TerrainType>();
-        TT.cube = cube; TT.bush = bush; TT.rock = rock; TT.tree = tree;
-        CombatGridCreator CGC = this.gameObject.AddComponent<CombatGridCreator>();
-        CGC.cube = cube; CGC.gridcube = gridCube;
-        grid = CGC.makeGrid();
-        if(grid!= null) { Debug.Log("got grid"); } else { Debug.Log("didn't get grid!!"); }
-        Destroy(TT);
-        Destroy(CGC);
-        for(int x = 0;x<gridSizeX; x++)
-        {
-            for (int y = 0; y < gridSizeZ; y++)
-            {
-                bestPaths[x, y] = new Queue<Vector2Int>();
-            }
-        }
-
-
-        if (Unit1 != null)
-        {
-            int U1x = 3, U1z = 3;
-            Transform TU1 = grid[U1x, U1z].GetComponent<cubeScript>().Node.transform;
-            GameObject U1 = (GameObject)Instantiate(Unit1, TU1.position, TU1.rotation);
-            U1.GetComponent<UnitControllerInterface>().setGrid(this, new Vector2Int(U1x, U1z));
-        }
-        if(Unit2 != null)
-        {
-            int U2x = 5, U2z = 3;
-            Transform TU2 = grid[U2x, U2z].GetComponent<cubeScript>().Node.transform;
-            GameObject U2 = (GameObject)Instantiate(Unit2, TU2.position, TU2.rotation);
-            U2.GetComponent<UnitControllerInterface>().setGrid(this, new Vector2Int(U2x, U2z));
-        }
-        if (Unit3 != null)
-        {
-            int U3x = 5, U3z = 5;
-            Transform TU3 = grid[U3x, U3z].GetComponent<cubeScript>().Node.transform;
-            GameObject U3 = (GameObject)Instantiate(Unit3, TU3.position, TU3.rotation);
-            U3.GetComponent<UnitControllerInterface>().setGrid(this, new Vector2Int(U3x, U3z));
-        }
-        if (Unit4 != null)
-        {
-            int U4x = 3, U4z = 5;
-            Transform TU4 = grid[U4x, U4z].GetComponent<cubeScript>().Node.transform;
-            GameObject U4 = (GameObject)Instantiate(Unit4, TU4.position, TU4.rotation);
-            U4.GetComponent<UnitControllerInterface>().setGrid(this, new Vector2Int(U4x, U4z));
-        }
-        if(TickManager.instance == null)
-        {
-            this.gameObject.AddComponent<TickManager>();
-        }
-        if(!noTimer)
-            TickManager.instance.StartTicking(tickDelay);
-        else TickManager.instance.StartTicking(0);
+        startCombat();
 
     }
 
@@ -133,10 +52,6 @@ public class ArrayScriptCombat : MonoBehaviour, MapInterface
 
 
     public Queue<Vector2Int> getPath(int x, int z) { return bestPaths[x, z]; }
-    //public GameObject[,] getGrid()
-    //{
-    //    return grid;
-    //}
     public void unHighlight()
     {
         for (int x = 0; x < gridSizeX; x++)
@@ -169,11 +84,7 @@ public class ArrayScriptCombat : MonoBehaviour, MapInterface
     private void highlightGrid(int x, int z, Color C, int count, Queue<Vector2Int> path)
     {
         if (x < 0 || x >= gridSizeX || z < 0 || z >= gridSizeZ || count <= 0) return;
-        //Debug.Log("path so far:");
-        //foreach(Transform T in path)
-        //{
-        //    Debug.Log("Transform: " + T);
-        //}
+        
 
         {
             cubeScript tcs = grid[x, z].GetComponent<cubeScript>();
@@ -185,8 +96,7 @@ public class ArrayScriptCombat : MonoBehaviour, MapInterface
             tcs.selected(C);
         }
         if ((bestPaths[x, z].Count == 0) || (bestPaths[x, z].Count > path.Count)) bestPaths[x, z] = path;
-
-        //Debug.Log("Highlighted " + x + "," + z + " Count:"+count);
+        
 
         highlightGrid(x, z + 1, C, count - 1, new Queue<Vector2Int>(path));
         highlightGrid(x + 1, z, C, count - 1, new Queue<Vector2Int>(path));
@@ -237,32 +147,61 @@ public class ArrayScriptCombat : MonoBehaviour, MapInterface
         //black out the screen on the camera
         //coroutine 2 seconds loading time
         //camera back to normal, begin combat
-        //set timescale to 0
+        //set timescale to 0    <-this loading time can be completed by instead waiting to call TickManager.StartTicking()
+
+        TerrainType TT = this.gameObject.AddComponent<TerrainType>();
+        TT.cube = cube; TT.bush = bush; TT.rock = rock; TT.tree = tree;
+        CombatGridCreator CGC = this.gameObject.AddComponent<CombatGridCreator>();
+        CGC.cube = cube; CGC.gridcube = gridCube;
+        grid = CGC.makeGrid();
+        if (grid != null) { Debug.Log("got grid"); } else { Debug.Log("didn't get grid!!"); }
+        Destroy(TT);
+        Destroy(CGC);
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeZ; y++)
+            {
+                bestPaths[x, y] = new Queue<Vector2Int>();
+            }
+        }
+
+
+        if (Unit1 != null)
+        {
+            int U1x = 3, U1z = 3;
+            Transform TU1 = grid[U1x, U1z].GetComponent<cubeScript>().Node.transform;
+            GameObject U1 = (GameObject)Instantiate(Unit1, TU1.position, TU1.rotation);
+            U1.GetComponent<UnitControllerInterface>().setGrid(this, new Vector2Int(U1x, U1z));
+        }
+        if (Unit2 != null)
+        {
+            int U2x = 5, U2z = 3;
+            Transform TU2 = grid[U2x, U2z].GetComponent<cubeScript>().Node.transform;
+            GameObject U2 = (GameObject)Instantiate(Unit2, TU2.position, TU2.rotation);
+            U2.GetComponent<UnitControllerInterface>().setGrid(this, new Vector2Int(U2x, U2z));
+        }
+        if (Unit3 != null)
+        {
+            int U3x = 5, U3z = 5;
+            Transform TU3 = grid[U3x, U3z].GetComponent<cubeScript>().Node.transform;
+            GameObject U3 = (GameObject)Instantiate(Unit3, TU3.position, TU3.rotation);
+            U3.GetComponent<UnitControllerInterface>().setGrid(this, new Vector2Int(U3x, U3z));
+        }
+        if (Unit4 != null)
+        {
+            int U4x = 3, U4z = 5;
+            Transform TU4 = grid[U4x, U4z].GetComponent<cubeScript>().Node.transform;
+            GameObject U4 = (GameObject)Instantiate(Unit4, TU4.position, TU4.rotation);
+            U4.GetComponent<UnitControllerInterface>().setGrid(this, new Vector2Int(U4x, U4z));
+        }
+        if (TickManager.instance == null)
+        {
+            this.gameObject.AddComponent<TickManager>();
+        }
+        if (!noTimer)
+            TickManager.instance.StartTicking(tickDelay);
+        else TickManager.instance.StartTicking(0);
     }
-
-    //public bool setMaterial(int x, int y, Material m)
-    //{
-    //    if (check(x,y))
-    //    {
-    //        Jset(x, y, m);
-    //        return true;
-    //    }
-    //    else
-    //        return false;
-    //}
-
-    //private void Jset(Vector2Int pos, Material m) { Jset(pos.x, pos.y, m); }
-    //private void Jset(int x, int y, Material m)
-    //{
-    //    try
-    //    {
-    //        mapM[x, y].material = m;
-    //    }
-    //    catch
-    //    {
-    //        Debug.Log("Error happened in setting the map to " + m + " @ " + x +"," + y);
-    //    }
-    //}
 
 
 }
