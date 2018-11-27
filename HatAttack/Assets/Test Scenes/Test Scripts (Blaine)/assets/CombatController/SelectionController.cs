353ad917da4a0c01d78a271ded24334610958607
killerzombi <flamester41@gmail.com>
2018-11-27 16:47:21 -0500

    done in class -BH

--- HatAttack/Assets/Test Scenes/Test Scripts (Blaine)/assets/CombatController/SelectionController.cs
+++ HatAttack/Assets/Test Scenes/Test Scripts (Blaine)/assets/CombatController/SelectionController.cs
@@ -6,180 +6,182 @@ using UnityEngine;
 public class SelectionController : MonoBehaviour
 {
 
-  [SerializeField] private KeyCode click = KeyCode.Mouse0;
-  [SerializeField] private KeyCode escapeKey = KeyCode.F;
-  [SerializeField] private KeyCode tickNow = KeyCode.Space;
-  [SerializeField] private KeyCode backTick = KeyCode.LeftShift;
-  [SerializeField] private float range = 100f;
-  [SerializeField] private float HighlightStrength = .1f;
-  [SerializeField] private Color USelectColor;
-  [SerializeField] private Color ESelectColor;
-  [SerializeField] private GameObject ChoiceUI;
+    [SerializeField] private KeyCode click = KeyCode.Mouse0;
+    [SerializeField] private KeyCode escapeKey = KeyCode.F;
+    [SerializeField] private KeyCode tickNow = KeyCode.Space;
+    [SerializeField] private KeyCode backTick = KeyCode.LeftShift;
+    [SerializeField] private float range = 100f;
+    [SerializeField] private float HighlightStrength = .1f;
+    [SerializeField] private Color USelectColor;
+    [SerializeField] private Color ESelectColor;
+    [SerializeField] private GameObject ChoiceUI;
 
-  [SerializeField] private GameObject AttackChoiceUI;
+    [SerializeField] private GameObject AttackChoiceUI;
 
 
-  public MapInterface MInterface;  // to be removed
+    public MapInterface MInterface;  // to be removed
 
-  private SelectionInterface selected = null;
-  private Queue<SelectionInterface> S2;
-  private bool watching = true;
-  private CameraScript CS = null;
-  private UnitControllerInterface UCI = null, UCI2 = null;
+    private SelectionInterface selected = null;
+    private Queue<SelectionInterface> S2;
+    private bool watching = true;
+    private CameraScript CS = null;
+    private UnitControllerInterface UCI = null, UCI2 = null;
 
-  UnitControllerInterface cUCI;
-  private int roundForward = 0;
+    UnitControllerInterface cUCI;
+    private int roundForward = 0;
 
-  public void startMovement(bool t)
-  {
-    if (ChoiceUI != null) ChoiceUI.SetActive(false);
-    startMouse();
-    if (UCI != null && t)
+    public void startMovement(bool t)
     {
-      UCI.MoveUnit(UCI2.pathFrom(selected.getPosition()), roundForward);
-    }
-  }
-
-  public void startAttack(bool t)
-  {
-    if (AttackChoiceUI != null) AttackChoiceUI.SetActive(false);
-    if (UCI != null && t)
-    {
-      UCI.AttackUnit(cUCI.getSelectedUnit());
-    }
-  }
-  // Use this for initialization
-  void Start()
-  {
-    if (CS == null) CS = this.GetComponent<CameraScript>();
-    if (CS == null) Debug.Log("selectionscript cannot access camerascript");
-    S2 = new Queue<SelectionInterface>();
-  }
-  // Update is called once per frame
-  void Update()
-  {
-    if (Input.GetKeyDown(tickNow) && watching) if (TickManager.instance != null) TickManager.instance.tickNow();
-    if (Input.GetKeyDown(backTick) && watching) if (TickManager.instance != null) TickManager.instance.backTick();
-
-    if (Input.GetKeyDown(click) && watching)
-    {
-      RaycastHit hitRay = new RaycastHit();
-      if (Physics.Raycast(transform.position, transform.forward, out hitRay, range))
-      {
-        //Debug.Log("clicked: " + hitRay.collider.gameObject.name);
-        SelectionInterface SI = hitRay.collider.GetComponent<SelectionInterface>();
-        UnitControllerInterface cUCI = hitRay.collider.GetComponent<UnitControllerInterface>();
-
-        if (SI != null)
+        if (ChoiceUI != null) ChoiceUI.SetActive(false);
+        if (AttackChoiceUI != null) AttackChoiceUI.SetActive(false);
+        startMouse();
+        if (UCI != null && t)
         {
-          if (selected == null)
-          {
-            selected = SI;
-            startMouse();
-            selected.selected(USelectColor * HighlightStrength);
-            //Debug.Log("Selected 1");
-            if (cUCI != null)
+            UCI.MoveUnit(UCI2.pathFrom(selected.getPosition()), roundForward);
+        }
+    }
+
+    public void startAttack(bool t)
+    {
+        if (AttackChoiceUI != null) AttackChoiceUI.SetActive(false);
+        if (ChoiceUI != null) ChoiceUI.SetActive(false);
+        if (UCI != null && t)
+        {
+            UCI.AttackUnit(cUCI.getSelectedUnit());
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
+        if (CS == null) CS = this.GetComponent<CameraScript>();
+        if (CS == null) Debug.Log("selectionscript cannot access camerascript");
+        S2 = new Queue<SelectionInterface>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(tickNow) && watching) if (TickManager.instance != null) TickManager.instance.tickNow();
+        if (Input.GetKeyDown(backTick) && watching) if (TickManager.instance != null) TickManager.instance.backTick();
+
+        if (Input.GetKeyDown(click) && watching)
+        {
+            RaycastHit hitRay = new RaycastHit();
+            if (Physics.Raycast(transform.position, transform.forward, out hitRay, range))
             {
-              UCI = cUCI;
-              UCI.highlightGrid(USelectColor * HighlightStrength * 0.8f);
-            }
-          }
-          else
-          {
-            S2.Enqueue(SI);
-            if (cUCI != null)
-            {
-              showUI();
-              showAttackUI();
-              stopMouse();
-              SI.selected(ESelectColor * HighlightStrength);
-              UCI2 = cUCI;
+                //Debug.Log("clicked: " + hitRay.collider.gameObject.name);
+                SelectionInterface SI = hitRay.collider.GetComponent<SelectionInterface>();
+                UnitControllerInterface cUCI = hitRay.collider.GetComponent<UnitControllerInterface>();
+
+                if (SI != null)
+                {
+                    if (selected == null)
+                    {
+                        selected = SI;
+                        startMouse();
+                        selected.selected(USelectColor * HighlightStrength);
+                        //Debug.Log("Selected 1");
+                        if (cUCI != null)
+                        {
+                            UCI = cUCI;
+                            UCI.highlightGrid(USelectColor * HighlightStrength * 0.8f);
+                        }
+                    }
+                    else
+                    {
+                        S2.Enqueue(SI);
+                        if (cUCI != null)
+                        {
+                            //showUI();
+                            showAttackUI();
+                            stopMouse();
+                            SI.selected(ESelectColor * HighlightStrength);
+                            UCI2 = cUCI;
+                        }
+                        else
+                        {
+                            //if(cgc!=null)
+                            //{ 
+                            //Queue<Transform> path = cgc.getPath(SI.getPosition().x, SI.getPosition().y);
+                            //foreach (Transform T in path)
+                            //{
+                            //    Debug.Log(T.position);
+                            //}}
+                            //Debug.Log(SI.getPosition());
+                            startMouse();
+                            if (UCI != null)
+                            {
+                                UCI.MoveUnit(SI.getPosition(), roundForward);
+                                UCI.unHighlightGrid();
+                                roundForward++;
+                                UCI.highlightGrid(USelectColor * HighlightStrength * 0.8f, SI.getPosition(), roundForward);
+
+
+                                //if (selected != null) selected.deselected();
+                                //while (S2.Count > 0) S2.Dequeue().deselected();
+                                //selected = null; UCI = null; S2 = new Queue<SelectionInterface>();
+                                //if (CS != null)
+                                //{
+                                //    CS.startMovement();
+                                //}
+                                //else { Debug.Log("selectionscript cannot access camerascript"); }
+                            }
+                        }
+                        //Debug.Log("Selected 2");
+                    }
+                }
+                else
+                {
+                    ResetValues();
+                }
             }
             else
             {
-              //if(cgc!=null)
-              //{ 
-              //Queue<Transform> path = cgc.getPath(SI.getPosition().x, SI.getPosition().y);
-              //foreach (Transform T in path)
-              //{
-              //    Debug.Log(T.position);
-              //}}
-              //Debug.Log(SI.getPosition());
-              startMouse();
-              if (UCI != null)
-              {
-                UCI.MoveUnit(SI.getPosition(), roundForward);
-                UCI.unHighlightGrid();
-                roundForward++;
-                UCI.highlightGrid(USelectColor * HighlightStrength * 0.8f, SI.getPosition(), roundForward);
-
-
-                //if (selected != null) selected.deselected();
-                //while (S2.Count > 0) S2.Dequeue().deselected();
-                //selected = null; UCI = null; S2 = new Queue<SelectionInterface>();
-                //if (CS != null)
-                //{
-                //    CS.startMovement();
-                //}
-                //else { Debug.Log("selectionscript cannot access camerascript"); }
-              }
+                ResetValues();
             }
-            //Debug.Log("Selected 2");
-          }
         }
-        else
+        else if (Input.GetKeyDown(escapeKey) && watching)
         {
-          ResetValues();
+            ResetValues();
         }
-      }
-      else
-      {
-        ResetValues();
-      }
+
     }
-    else if (Input.GetKeyDown(escapeKey) && watching)
+
+    private void ResetValues()
     {
-      ResetValues();
+        if (selected != null) selected.deselected();
+        while (S2.Count > 0) S2.Dequeue().deselected();
+        selected = null; UCI = UCI2 = null; S2 = new Queue<SelectionInterface>();
+        roundForward = 0;
+        startMouse();
+        //Debug.Log("Selected 0");
     }
 
-  }
-
-  private void ResetValues()
-  {
-    if (selected != null) selected.deselected();
-    while (S2.Count > 0) S2.Dequeue().deselected();
-    selected = null; UCI = UCI2 = null; S2 = new Queue<SelectionInterface>();
-    roundForward = 0;
-    startMouse();
-    //Debug.Log("Selected 0");
-  }
-
-  private void showUI(GameObject UI = null)
-  {
-    if (UI == null) UI = ChoiceUI;
-    if (UI != null) UI.SetActive(true);
-  }
-
-  private void showAttackUI(GameObject UI = null)
-  {
-    if (UI == null) UI = AttackChoiceUI;
-    if (UI != null) UI.SetActive(true);
-  }
-
-  private void stopMouse()
-  {
-    if (CS != null)
+    private void showUI(GameObject UI = null)
     {
-      CS.stopMovement();
+        if (UI == null) UI = ChoiceUI;
+        if (UI != null) UI.SetActive(true);
     }
-    else { Debug.Log("selectionscript cannot access camerascript"); }
-  }
-  private void startMouse()
-  {
-    if (CS != null)
+
+    private void showAttackUI(GameObject UI = null)
     {
-      CS.startMovement();
+        if (UI == null) UI = AttackChoiceUI;
+        if (UI != null) UI.SetActive(true);
     }
-    else { Debug.Log("selectionscript cannot access camerascript"); }
-  }
+
+    private void stopMouse()
+    {
+        if (CS != null)
+        {
+            CS.stopMovement();
+        }
+        else { Debug.Log("selectionscript cannot access camerascript"); }
+    }
+    private void startMouse()
+    {
+        if (CS != null)
+        {
+            CS.startMovement();
+        }
+        else { Debug.Log("selectionscript cannot access camerascript"); }
+    }
 }
--- HatAttack/Assets/Test Scenes/Test Scripts (Blaine)/scripts/interfaces/UnitControllerInterface.cs
+++ HatAttack/Assets/Test Scenes/Test Scripts (Blaine)/scripts/interfaces/UnitControllerInterface.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public interface UnitControllerInterface
 {
 
-  void backMove(Queue<Vector2Int> origPath, Vector2Int target);
-  void MoveUnit(Queue<Vector2Int> path, int TicksForward = 0);
-  void MoveUnit(Vector2Int target, int TicksForward = 0);
-  void setGrid(MapInterface cgc, Vector2Int pos);
-  void highlightGrid(Color C, int ticksForward = 0);
-  void highlightGrid(Color C, Vector2Int pos, int ticksForward = 0);
-  void unHighlightGrid();
-  float AttackUnit(GameObject target);
+    void backMove(Queue<Vector2Int> origPath, Vector2Int target);
+    void MoveUnit(Queue<Vector2Int> path, int TicksForward = 0);
+    void MoveUnit(Vector2Int target, int TicksForward = 0);
+    void setGrid(MapInterface cgc, Vector2Int pos);
+    void highlightGrid(Color C, int ticksForward = 0);
+    void highlightGrid(Color C, Vector2Int pos, int ticksForward = 0);
+    void unHighlightGrid();
+    float AttackUnit(GameObject target);    //returns ctual damage done, NOT attack of unit
+    float UnitAttacked(GameObject attacker, float damage); //returns damage taken
+    float unAttacked(GameObject attacker, float damage);
+    float unAttack(GameObject target);
+    void getEXP(float exp);
+    void ungetEXP(float exp);
 
-  GameObject getSelectedUnit();
-  Queue<Vector2Int> pathFrom(Vector2Int startingPoint);
+    GameObject getSelectedUnit();
+    Queue<Vector2Int> pathFrom(Vector2Int startingPoint);
 }
