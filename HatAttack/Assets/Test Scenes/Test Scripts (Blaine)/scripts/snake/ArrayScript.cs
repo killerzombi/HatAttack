fb7cbd7b91c05c8e53c93fae00f699f0632693b8
evan0101 <supdeerlord@gmail.com>
2018-11-28 14:13:43 -0500

    Added sprites for Snake + Fixed a few syntax errs in ACS.cs -EM
    Added sprites for snake in UI > Snake, and also fixed a few missing semi colons in ArrayScriptCombat, though there is still a few errors I wasn't able to figure out!

---
+++ HatAttack/Assets/Test Scenes/Test Scripts (Blaine)/scripts/snake/ArrayScript.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayScript : MonoBehaviour, SnakeMapInterface
+{
+    public GameObject prefabCube;
+    public Material snakeMaterial;
+    public Material transparent;
+    public Material next;
+    public Material food;
+    public Material boundary;
+    public Material dangerNext;
+    public int width, height;
+    public float spacing = .25f;  // space between cubes
+    public float tickDelay = 3f;
+    public bool easyMode = false;
+    
+    private GameObject[,] map;
+    private MeshRenderer[,] mapM;
+    private bool[,] danger;  //is this cube  snake?
+    private int MW, ML;  //map length and width
+    private Material nextBack;
+    private Vector2Int NBposition;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        nextBack = null;
+        Vector2 topLeft = new Vector2((float)-((width + ((width - 1) * spacing)) / 2), (float)((height + ((height - 1) * spacing)) / 2));
+        ML = width;
+        MW = height;
+        Debug.Log("Array: H:" + height + "  W:" + width + "  ML:" + ML + "  MW:" + MW);
+        map = new GameObject[width, height];
+        mapM = new MeshRenderer[ML, MW];
+        danger = new bool[ML, MW];
+        for(int x = 0; x< width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                map[x, y] = (GameObject)Instantiate(prefabCube, new Vector3(topLeft.x + x + x * spacing, 0f, topLeft.y - y - y * spacing), Quaternion.identity, this.gameObject.transform);
+                mapM[x, y] = map[x, y].GetComponent<MeshRenderer>();
+                danger[x, y] = false;
+            }
+        }
+        
+        foreach (MeshRenderer M in mapM)
+        {
+            M.material = transparent;
+        }
+        StartSnake();
+    }
+
+    void StartSnake()
+    {
+        SnakeScript SC = this.gameObject.AddComponent<SnakeScript>();
+        SC.StartSnake(height, width, this, tickDelay, easyMode);
+    }
+
+    #region interface
+    public void Revert(int x, int y)
+    {
+        Jset(x, y, transparent);
+        danger[x, y] = false;
+    }
+
+    public void Become(int x, int y)    //only called when moving to next? assumption made = true!
+    {
+        nextBack = snakeMaterial;       //Based on assumption == true
+        Jset(x, y, snakeMaterial);
+        danger[x, y] = true;
+    }
+
+    public void BecomeNext(int x, int y)
+    {
+        setNext(x, y);
+    }
+
+    public void setFood(int x, int y)
+    {
+        Jset(x, y, food);
+    }
+
+    public void SetBoundary(int x, int y)
+    {
+        Jset(x, y, boundary);
+        danger[x, y] = true;
+    }
+    #endregion
+
+    private void setNext(int x, int y)
+    {
+        if (nextBack != null)
+        {
+            Jset(NBposition, nextBack);
+        }
+        NBposition = new Vector2Int(x, y);
+        try
+        {
+            nextBack = mapM[x, y].material;
+        }
+        catch
+        {
+            Debug.Log("nextBack setting to mapM[" + x + "," + y + "].material  had an error!");
+        }
+        if (danger[x, y])
+            Jset(x,y,dangerNext);
+        else
+            Jset(x, y, next);
+    }
+
+    private void Jset(Vector2Int pos, Material m) { Jset(pos.x, pos.y, m); }
+    private void Jset(int x, int y, Material m)
+    {
+        try
+        {
+            mapM[x, y].material = m;
+        }
+        catch
+        {
+            Debug.Log("Error happened in setting the map to " + m + " @ " + x +"," + y);
+        }
+    }
+
+          
+}
---
+++ HatAttack/Assets/Test Scenes/Test Scripts (Blaine)/scripts/snake/SnakeMapInterface.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface SnakeMapInterface {
+
+    void Revert(int x, int y);
+    void Become(int x, int y);
+    void BecomeNext(int x,int y);
+    void SetBoundary(int x, int y);
+    void setFood(int x, int y);
+}
