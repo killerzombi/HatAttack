733ad0e43150982a01c90deb32ae9f2f237548d3
killerzombi <flamester41@gmail.com>
2018-11-28 18:14:12 -0500

    Health Bars! -BH
    also added CombatInterface for overworld player to interact with
    --Has EnqueuePlayer, EnqueueEnemy, and an event that will be subscriibed to as seen below with making the functionToRun run when combat ends.
    CombatInterface CI; CI.endOfCombat += functionToRun
    ++currently functionToRun must accept parameters (Stack<GameObjects> captured)
    ++++this can be extended for more parameters on the CombatInterface.cs

---
+++ HatAttack/Assets/Combat Scene/Scripts/FollowPosition.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPosition : MonoBehaviour {
+    [SerializeField] private Transform PositionToFollow;
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = PositionToFollow.position;
+	}
+}
---
+++ HatAttack/Assets/Combat Scene/Scripts/land unit/HealthControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthControl : MonoBehaviour {
+    [SerializeField] private Image healthBar;
+	// Use this for initialization
+	void Start () {
+        if (healthBar == null) Debug.Log("Missing healthbar");
+	}
+	
+    //fill between 0 and 1
+    public void setHealth(float fill) { if(fill >= 0 && fill <=1) healthBar.fillAmount = fill; }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
