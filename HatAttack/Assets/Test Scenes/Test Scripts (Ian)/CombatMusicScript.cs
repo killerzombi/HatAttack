d554e3ee26ff177393d98418e6a79a2637fb8e16
AinTheBrain <Ian818@yahoo.com>
2018-11-29 15:20:25 -0500

    IR worked on Music Loops

---
+++ HatAttack/Assets/Test Scenes/Test Scripts (Ian)/CombatMusicScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatMusicScript : MonoBehaviour {
+
+    public AudioClip MusicIntro;
+    public AudioClip MusicLoop;
+
+    public float IntroDuration = 8.4f;
+    public float LoopDuration = 82.3f;
+
+    private AudioSource play;
+    bool loop = false;
+
+    // Use this for initialization
+    void Start () {
+        play = GetComponent<AudioSource>();
+        StartCoroutine(Music());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (loop)
+        {
+            StartCoroutine(Loop());
+        }
+	}
+
+    IEnumerator<WaitForSeconds> Music()
+    {
+        play.PlayOneShot(MusicIntro);
+        yield return new WaitForSeconds(8.4f);
+        loop = true;
+
+    }
+    IEnumerator<WaitForSeconds> Loop()
+    {
+        loop = false;
+        play.PlayOneShot(MusicLoop);
+        yield return new WaitForSeconds(82.3f);
+        loop = true;
+    }
+}
