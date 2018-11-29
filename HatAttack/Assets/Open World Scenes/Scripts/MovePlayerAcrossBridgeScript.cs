23528a21a78a8b91fd329893c175ebe60bfbaeeb
erik <erik.m.english@gmail.com>
2018-11-29 17:23:00 -0500

    Revert "dont know"
    This reverts commit 64aa790380a950fcc2c55d0c0711f78939706d6f.

--- HatAttack/Assets/Open World Scenes/Scripts/MovePlayerAcrossBridgeScript.cs
+++ HatAttack/Assets/Open World Scenes/Scripts/MovePlayerAcrossBridgeScript.cs
@@ -19,23 +19,13 @@ public class MovePlayerAcrossBridgeScript : MonoBehaviour {
 	{
 		if (collisionInfo.gameObject.tag == "Bridge")
 		{
-<<<<<<< HEAD
 			tempTrans = transform.parent;
 			Debug.Log("collided with:" + collisionInfo);
-=======
-            
-			tempTrans = transform.parent;
-			Debug.Log("collided with:" + collisionInfo.gameObject);
->>>>>>> parent of 929ca8ab... Merge branch 'master' of https://github.com/killerzombi/HatAttack
 			transform.parent = collisionInfo.gameObject.transform;
 		}
 	}
 	void OnCollisionExit(Collision collisionInfo)
 	{
-<<<<<<< HEAD
-=======
-        Debug.Log("stopped colliding with " + collisionInfo.gameObject);
->>>>>>> parent of 929ca8ab... Merge branch 'master' of https://github.com/killerzombi/HatAttack
 		if (collisionInfo.gameObject.tag == "Bridge")
 			transform.parent = tempTrans;
 	}
