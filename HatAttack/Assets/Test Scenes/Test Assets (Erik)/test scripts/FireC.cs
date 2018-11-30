b136dd31dac2e34de3ac7d53f7db9f85351481f3
CyborgDuck <erik.m.english@gmail.com>
2018-11-30 12:05:41 -0500

    maybe its not completely broken after all?
    redid changes I did in class yesterday.

---
+++ HatAttack/Assets/Test Scenes/Test Assets (Erik)/test scripts/FireC.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireC : MonoBehaviour {
+
+    public GameObject[] iceSpawn;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("collision!!" + other.name);
+        if (other.gameObject.tag == "Icebeam")
+        {
+            foreach (GameObject g in GameObject.FindGameObjectsWithTag("FireC"))
+            {
+                GameObject.FindWithTag("FireC").SetActive(false);
+            }
+
+
+
+
+        }
+
+    }
+}
---
+++ HatAttack/Assets/Test Scenes/Test Assets (Erik)/test scripts/FireD.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireD : MonoBehaviour {
+
+
+    public GameObject[] iceSpawn;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("collision!!" + other.name);
+        if (other.gameObject.tag == "Icebeam")
+        {
+            foreach (GameObject g in GameObject.FindGameObjectsWithTag("FireD"))
+            {
+                GameObject.FindWithTag("FireD").SetActive(false);
+            }
+
+
+
+
+        }
+
+
+
+    }
+
+}
+
