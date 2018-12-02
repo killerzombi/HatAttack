e216485bc45c9396bb23e4de738b52bd7bccbb08
evan0101 <supdeerlord@gmail.com>
2018-12-02 15:24:21 -0500

    Current Units now show up in Inventory -EM

--- HatAttack/Assets/UI/Scripts/InventoryHandler.cs
+++ HatAttack/Assets/UI/Scripts/InventoryHandler.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryHandler : MonoBehaviour
 {
-
+    public TickManager tm;
     bool isOpen = false;
+    public Canvas canvasGO;
+    private Queue<GameObject> unitQueue;
+    private GameObject[] unitList;
+    private Text text;
 
 
-  public GameObject Inventory;
+    public GameObject Inventory;
 
 
     void closeInventory()
@@ -28,8 +33,19 @@ public class InventoryHandler : MonoBehaviour
         else
         {
             Inventory.SetActive(true);
-                isOpen = true;
-        }
+            isOpen = true;
+            unitQueue = tm.getInitiativeList();
+            unitList = new GameObject[unitQueue.Count];
+            unitQueue.CopyTo(unitList, 0);
+
+            // Loop through the queue
+            for (int x = 0; x < unitList.Length; x++)
+            {
+                text = GameObject.Find("txt_v_inventory_unit" + x).GetComponent<Text>();
+                text.text = unitList[x] + "";
+            }
+            }
+
     }
   }
 
