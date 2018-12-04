390a6442a6e72a3e44baffd00d3422d8fcb4e058
voyix <Nathan.Baines64@gmail.com>
2018-12-03 21:28:41 -0500

    Moved some assets around, deleted some things that weren't in use - NB
    revert this change if anything important got deleted, otherwise, most things just got moved into different sub folders and out of the main assets folder.

---
+++ HatAttack/Assets/Audio Files/Audio Scripts/QuitMusicScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitMusicScript : MonoBehaviour {
+
+    public MusicScript musicScipt;
+	// Use this for initialization
+	void Start () {
+        musicScipt.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ HatAttack/Assets/Open World Scenes/Scripts/SwitchScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchScript : MonoBehaviour {
+
+    public Animator IceGiantAnim;
+    public Animator DemonLordAnim;
+    public Animator PlayerAnim;
+    //Animator BirdAnim;
+
+    public IceGiantScript iceGiantScript;
+    public DemonLordScript demonLordScript;
+    public spawnIceBlock IceBlockScript;
+    //BirdScript birdScript;
+
+    public SkinnedMeshRenderer IceGiantRenderer;
+    public SkinnedMeshRenderer DemonLordRenderer;
+    //SkinnedMeshRenderer BirdRenderer;
+    int creatureType = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Q))
+        {
+            creatureType--;
+            if (creatureType == -1)
+            {
+                creatureType = 1;
+            }
+            changeCreature();
+        }
+	}
+
+    void changeCreature()
+    {
+        IceGiantAnim.enabled = false;
+        iceGiantScript.enabled = false;
+        IceGiantRenderer.enabled = false;
+        IceBlockScript.enabled = false;
+
+        DemonLordAnim.enabled = false;
+        demonLordScript.enabled = false;
+        DemonLordRenderer.enabled = false;
+        PlayerAnim.enabled = false;
+
+        //BirdAnim.enabled = false;
+        //birdScript.enabled = false;
+        //BirdRenderer.enabled = false;
+
+        if (creatureType == 0)
+        {
+            IceGiantAnim.enabled = true;
+            iceGiantScript.enabled = true;
+            IceGiantRenderer.enabled = true;
+            IceBlockScript.enabled = true;
+        }
+        else if (creatureType == 1)
+        {
+            DemonLordAnim.enabled = true;
+            demonLordScript.enabled = true;
+            DemonLordRenderer.enabled = true;
+            PlayerAnim.enabled = true;
+        }
+    }
+}
